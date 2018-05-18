3ed7abdd726c674e746d802196d874e1ab8b94d7
Unknown <16171281@itculiacan.edu.mx>
2018-05-17 23:01:32 -0600

    home.js

--- home.js
+++ home.js
@@ -10,15 +10,30 @@ const HomeView = class HomeScreen extends React.Component {
     render() {
       const { navigate } = this.props.navigation;
       return (
-        <Button
-          title="Go to Jane's profile"
+        <View style={styles.container}>
+        <View styler={styles.containerLogo}><Text > hello world!</Text></View>
+        <View style={styles.containerButton}>
+        <Button 
+          title="Sing Up!"
           onPress={() =>
             navigate('Profile', { name: 'Jane' })
           }
         />
+        
+        </View>
+        
+        </View>
       );
     }
   }
+const styles = StyleSheet.create({
+container:{ flex:1, backgroundColor: '#FFFFFF',justifyContent:'center',alignItems:'center'},
+containerLogo:{flex:1,justifyContent:'center',alignItems:'center'},
+containerButton:{flex: 1,width:300 ,justifyContent:'center',alignItems:'center'}
 
+
+
+
+});
 
   export default HomeView;
