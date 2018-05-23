9e16df53783b6b5d0237a173262c5745ffaec0cf
Unknown <16171281@itculiacan.edu.mx>
2018-05-22 19:45:56 -0600

    Auto stash before merge of "develop" and "origin/develop"

---
+++ views/signUp/CreatePinScreen.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet,Image,Button,TextInput  } from "react-native";
+
+
+class CreatePinScreen extends Component{
+    render(){
+        return (
+            <View style={styles.container}>
+            <View style={styles.textContainer}>
+                <Text style={styles.textStyle}> Create a PIN for accesing the application</Text>
+            </View>
+            <View style={styles.InputContainer}>
+            <TextInput style={{height: 80, width:400, borderColor: 'grey', borderWidth: 1}}></TextInput>
+            </View>
+            <View style={styles.ButtonsContainer}>
+                        <View style={styles.ButtonRow1}>
+                                <View style={[[styles.CircleShapeView,styles.Colum1],styles.Colum1]}></View>
+                                <View style={[[styles.CircleShapeView,styles.Colum1],styles.Colum2]}></View>
+                                <View style={[[styles.CircleShapeView,styles.Colum1],styles.Colum3]}></View>
+                                    
+                            
+                            </View> 
+
+
+                        <View style={styles.ButtonRow2}>
+                                <View style={[[styles.CircleShapeView,styles.Colum1],styles.Colum1]}></View>
+                                <View style={[[styles.CircleShapeView,styles.Colum2],styles.Colum1]}></View>
+                                <View style={[styles.CircleShapeView,styles.Colum3]}></View>
+                            
+                            
+                            </View> 
+
+
+                        <View style={styles.ButtonRow2}>
+                                <View style={[styles.CircleShapeView,styles.Colum1]}></View>
+                                <View style={[styles.CircleShapeView,styles.Colum2]}></View>
+                                <View style={[styles.CircleShapeView,styles.Colum3]}></View>
+                                    
+                            
+                            </View>     
+
+                            
+
+                    </View>
+        </View>
+          );
+    }
+} 
+export default CreatePinScreen
+
+const styles = StyleSheet.create({
+    container:{ flex:1, backgroundColor: '#FFFFFF',justifyContent:'center',alignItems:'center'},
+    textContainer:{flex:1, backgroundColor: '#FFFFFF',justifyContent:'flex-start',alignItems:'center'},
+    textStyle:{ fontSize: 30, justifyContent:'center',textAlign: 'center',color:'grey'},
+    InputContainer:{flex:1,backgroundColor:'#FFFFFF',justifyContent:'flex-start',alignItems:'center',width:400,marginRight:20},
+    ButtonsContainer:{flex:3,justifyContent: 'center',alignItems:'center',right:10,bottom:80},
+    CircleShapeView: { width: 75, height: 75, borderRadius: 150/2, backgroundColor: '#00BCD4',justifyContent:'space-around',alignItems:'stretch',right:20},
+    ButtonRow1:{flexDirection:'row',  justifyContent: 'space-around',alignItems:'stretch',paddingHorizontal:30,marginRight:30,marginTop:20 ,marginLeft:20, paddingLeft:20,paddingRight:20},
+    ButtonRow2:{flexDirection:'row',  justifyContent: 'space-around',alignItems:'stretch',paddingHorizontal:30,marginRight:30,marginTop:20,marginLeft:20,paddingLeft:20,paddingRight:20},
+    ButtonRow3:{flexDirection:'row',  justifyContent: 'space-around',alignItems:'stretch',paddingHorizontal:30,marginRight:30,marginTop:20,marginLeft:20,paddingLeft:20,paddingRight:20},
+    Colum1:{right:20},
+    Colum2:{left:20},
+    Colum3:{left:60},
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    });
--- views/signUp/TakeASelfieScreen2.js
+++ views/signUp/TakeASelfieScreen2.js
@@ -1,42 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet,Image,Button  } from "react-native";
 
-class ProfilePhotoScreen extends Component{
+class TakeASelfieScreen2 extends Component{
     render(){
         return (
             <View style={styles.container}>
               <View style={styles.logoContainer}>
               <View style={styles.titleContainer}>
-                <Text style={styles.titleText}>Your Pofile Photo</Text>
+                <Text style={styles.titleText}>Take a Selfie</Text>
                 </View>  
+              <View style={styles.instructionContainer}><Text style={styles.welcomeText}> Follow the instructions.......</Text>
+                </View>    
               </View>
               <View style={styles.bodyContainer}>
-            
     
-              </View>
-              
-            
-              <Image style={styles.imgIndicator}
-                  source={require('cle-passport/assets/icons/correct.png')}
-        />
-              
-              <Text style={styles.welcomeText}>Correct!</Text>
-
-              <View style={{ flexDirection: 'row', justifyContent:'space-between',alignItems:'flex-end'}}>
-              <Button style={styles.singupButton1} 
-              title="Retry"
-              onPress={() =>
-                navigate('Profile', { name: 'Jane' })
-              }
-            />
-             <Button style={styles.singupButton2} 
-              title="Continue"
-              onPress={() =>
-                navigate('Profile', { name: 'Jane' })
-              }
-            />
-              
-              
               </View>
               
 
@@ -46,25 +23,20 @@ class ProfilePhotoScreen extends Component{
     }
 } 
 
-export default ProfilePhotoScreen
+export default TakeASelfieScreen2
 
 const styles = StyleSheet.create({
     container:{ flex:2, backgroundColor: '#FFFFFF',justifyContent:'flex-start',alignItems:'center'},
-    logoContainer:{ flex:5, backgroundColor: '#FFFFFF',justifyContent:'center',alignItems:'center',width:'100%',bottom:30},
+    logoContainer:{ flex:5, backgroundColor: '#FFFFFF',justifyContent:'center',alignItems:'center',width:'100%'},
     imageContainer:{ flex:1, backgroundColor: '#FFFFFF',justifyContent:'center',alignItems:'center'},
     bodyContainer:{ flex:6, backgroundColor: '#c9c9c9',width:'90%', height:'100%',justifyContent:'flex-start',alignItems:'center',bottom:200},
     titleContainer:{ flex:1, backgroundColor: '#FFFFFF',width:'70%',justifyContent:'center',alignItems:'center',bottom:80},
     titleText:{fontSize:40, top:20},
     instructionContainer:{ flex:1, backgroundColor: '#FFFFFF',justifyContent:'center',alignItems:'stretch',bottom:100,width:'100%',left:20},
-    welcomeText:{fontSize:16 , bottom:190},
+    welcomeText:{fontSize:16 , bottom:80},
     indicatorContainer:{ flex:1, backgroundColor: '#FFFFFF',justifyContent:'flex-start',alignItems:'center'},
     welcomeindicatorContainer:{ flex:1, backgroundColor: '#FFFFFF',justifyContent:'center',alignItems:'stretch',bottom:40},
-    imgIndicator:{width:30,height:30,bottom:190},
-    singupButton1:{  backgroundColor: '#c9c9c9',justifyContent:'space-between' ,alignItems:'center',left:20},
-    singupButton1:{  backgroundColor: '#c9c9c9',justifyContent:'space-between' ,alignItems:'center',right:20},
-
-    
-
+    imgIndicator:{width:50,height:50},
 
     
     
