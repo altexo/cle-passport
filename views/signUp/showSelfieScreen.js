c9a4f3c7bedb8608e926490d76f85729e430d3fe
Alex Rios <altexo94@gmail.com>
2018-06-03 20:49:55 -0600

    Show selfie basic styles

--- views/signUp/showSelfieScreen.js
+++ views/signUp/showSelfieScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet,Image,Button,CameraRoll  } from "react-native";
+import { View, Text, StyleSheet,Image,Button,CameraRoll, TouchableOpacity  } from "react-native";
 import { Constants, FileSystem, Camera, Permissions,ImageManipulator } from 'expo';
 class showSelfieScreen extends React.Component{
 
@@ -10,6 +10,10 @@ class showSelfieScreen extends React.Component{
           console.log('Params: ')
           console.log(params.params)
           let imageURL = params.params
+          
+          // //Test
+          // let imageURL = 'content://media/external/images/media/21708'
+          // //EndTest
           this.setState({imageUri:{
               uri: imageURL
             }
@@ -23,15 +27,34 @@ class showSelfieScreen extends React.Component{
    
         return (
             <View style={styles.container}>
-              {/* <View style={styles.logoContainer}>
-              <View style={styles.titleContainer}>
-                <Text style={styles.titleText}>Take a Selfie</Text>
-                </View>  
-              <View style={styles.instructionContainer}><Text style={styles.welcomeText}>{params}</Text>
-                </View>    
+              <View style={{flex: 0.2}}>
+                <Text style={{fontSize: 30}}>Your profile photo</Text>
+              </View>
+              <View style={styles.imageContainer}>
+                <Image style={{width: 400, height:330}} source={this.state.imageUri}/>       
+              </View>
+           
+              {/* <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.buttonStyle}>
+                    <Text style={{color: 'white', textAlign: 'center'}}>RETRY</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.buttonStyle}>
+                    <Text style={{color: 'white', textAlign: 'center'}}>CONTINUE</Text>
+                  </TouchableOpacity>
+                  <Button style={{height:60,width: '100%'}} title="Retry"/>
+                  <Button style={{height:60,width: '100%'}} title="continue"/>
               </View> */}
-              <Image style={{flex: 1,flexDirection:'row', alignItems:'center', justifyContent: 'center', width: 200, height: 200}} source={this.state.imageUri}/>       
+              <View style={ styles.bottomView}>
+            
+ 
+                <TouchableOpacity style={styles.buttonStyle}>
+                    <Text style={{color: 'white', textAlign: 'right'}}>RETRY</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.buttonStyle}>
+                    <Text style={{color: 'white', textAlign: 'left'}}>CONTINUE</Text>
+                  </TouchableOpacity>
 
+              </View>
             
           
             </View>
@@ -43,17 +66,42 @@ class showSelfieScreen extends React.Component{
 export default showSelfieScreen
 
 const styles = StyleSheet.create({
-    container:{ flex:1, justifyContent:'flex-start',alignItems:'center'},
-    logoContainer:{ flex:5, backgroundColor: '#FFFFFF',justifyContent:'center',alignItems:'center',width:'100%'},
-    imageContainer:{ flex:1, backgroundColor: '#FFFFFF',justifyContent:'center',alignItems:'center'},
-    bodyContainer:{ flex:6, backgroundColor: '#c9c9c9',width:'90%', height:'100%',justifyContent:'flex-start',alignItems:'center',bottom:200},
-    titleContainer:{ flex:1, backgroundColor: '#FFFFFF',width:'70%',justifyContent:'center',alignItems:'center',bottom:80},
-    titleText:{fontSize:40, top:20},
-    instructionContainer:{ flex:5, backgroundColor: '#FFFFFF',justifyContent:'center',alignItems:'stretch',bottom:100,width:'100%',left:20},
-    welcomeText:{fontSize:16 , bottom:80},
-    indicatorContainer:{ flex:1, backgroundColor: '#FFFFFF',justifyContent:'flex-start',alignItems:'center'},
-    welcomeindicatorContainer:{ flex:1, backgroundColor: '#FFFFFF',justifyContent:'center',alignItems:'stretch',bottom:40},
-    imgIndicator:{width:50,height:50},
+    container:{ 
+      flex:1, 
+      //justifyContent:'flex-start',
+      alignItems:'center'
+    },
+    imageContainer:
+    { flex:2, 
+      alignItems: 'center',
+    
+    },
+    buttonContainer:
+    {
+      flex: 0.3,
+      flexDirection: 'row' ,
+      alignItems: 'stretch',
+      width: '100%'
+      
+    },
+    // buttonStyle:{
+    //  position: 'absolute',
+    //  bottom: 0,
+    //  alignItems: 'stretch'
+    //  // alignItems: 'flex-center'
+    // },
+    bottomView:{
+ 
+      width: '100%', 
+      height: 50, 
+      backgroundColor: '#0592fe', 
+      justifyContent: 'space-between', 
+      alignItems: 'center',
+      flexDirection: 'row' ,
+      position: 'absolute',
+      bottom: 0
+    },
+  
 
     
     
