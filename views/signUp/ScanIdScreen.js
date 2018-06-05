d4c2cf4cf972e17777000e5faca42ce958809c6e
Alex Rios <altexo94@gmail.com>
2018-06-04 23:45:19 -0600

    Modal in profile

--- views/signUp/ScanIdScreen.js
+++ views/signUp/ScanIdScreen.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Button, Image, TouchableOpacity  } from "react-native";
+import Modal from "react-native-modal";
 
 class ScanIdScreen extends Component{
+    state = {
+        isModalVisible: false
+      };
+    
+      _toggleModal = () =>
+        this.setState({ isModalVisible: !this.state.isModalVisible });
+    
     render(){
         return (
             <View style={styles.container}>
@@ -13,12 +21,30 @@ class ScanIdScreen extends Component{
                     />
                     </View>
                     <View style={{flex: 2}}>
+                    <Modal isVisible={this.state.isModalVisible} style={{height: 100}}>
+                        <View style={styles.modalContent}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <Image source={require('cle-passport/assets/icons/identificacion.png')}/>
+                            <Text style={{marginLeft: '5%', fontSize: 23, marginTop: 0}}>ID Type</Text>
+                        </View>
+                        
+                            <TouchableOpacity style={{}}>
+
+                                <Text style={{textAlign:'center', fontSize: 23, marginTop: '8%',marginBottom: '8%', borderWidth: 0.5,
+    borderColor: 'black',}}>INE</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={this._toggleModal}>
+                                <Text style={{textAlign: 'right'}}>Cancel</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </Modal>
                         <Text style={{fontSize: 30, textAlign: 'center', fontWeight: 'bold', marginBottom: '5%', marginTop: '5%'}}>Clé Passport</Text>
                         <Text style={{fontSize: 20, textAlign: 'center'}}>It look your Clé passport is incomplete</Text>
                     </View>
                </View>
                <View style={{alignItems: 'stretch', height: 0.4}}>
-                <TouchableOpacity style={styles.buttonStyles}>
+                <TouchableOpacity style={styles.buttonStyles}  onPress={this._toggleModal}>
+             
                     <Text style={{color: 'white', fontSize: 20}}>Scan Oficial ID</Text>
                 </TouchableOpacity>
                 {/* <Button style={{height: 90}} style={{position: 'absolute'}}title="Scan Oficial ID"/> */}
@@ -31,6 +57,7 @@ class ScanIdScreen extends Component{
                     <Image/><Text style={{color: 'white', fontSize: 20}}>Scan QR Code</Text>
                 </TouchableOpacity>
                </View>
+           
             </View>
         );
     }
@@ -73,4 +100,12 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
 
     },
+    modalContent: {
+        backgroundColor: 'white',
+        padding: 40,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        borderRadius: 4,
+        borderColor: 'rgba(0, 0, 0, 0.1)',
+      },
 });
