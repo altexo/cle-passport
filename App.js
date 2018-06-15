8a2b7a354653d7544e2ae544c755ddb1f01ce55c
Unknown <16171281@itculiacan.edu.mx>
2018-06-14 18:09:41 -0600

    botones y camara jalando!

--- App.js
+++ App.js
@@ -20,6 +20,7 @@ import ScanIdBack from './views/signUp/scanIdBack.js';
 import verifyidBack from './views/signUp/verifyidBack.js';
 import TakeASelfieScreen from './views/TakeASelfieScreen.js';
 import VerifyPinScreen from './views/signUp/VerifyPinScreen.js';
+import AmazonCognitoIdentity from 'amazon-cognito-identity-js';
 
 class ProfileScreen extends React.Component {
   static navigationOptions = {
@@ -62,60 +63,3 @@ const App = createStackNavigator({
 
 export default App;
 
-var IDENTITY_POOL_ID = "us-west-2:2b780d89-b164-404c-9d21-4148a6d3ee18";
-var CLIENT_ID = "7mkla4lrl0lu04jp6d9fqupa7e";
-var COGNITO_REGION = "us-west-2";
-var USER_POOL_ID = "us-west-2_w1b1FWdV8";
-
-
-
-var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
-
-
-var authenticationData = {
-  Username : 'justino',
-  Password : '+`4EbERa&"fN2rP"'
-};
-var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
-var poolData = {
-  UserPoolId : USER_POOL_ID,
-  ClientId : CLIENT_ID
-};
-var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-var userData = {
-  Username : 'justino',
-  Pool : IDENTITY_POOL_ID
-};
-var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-cognitoUser.authenticateUser(authenticationDetails, {
-  onSuccess: function (result) {
-      console.log('access token + ' + result.getAccessToken().getJwtToken());
-
-      //POTENTIAL: Region needs to be set if not already set previously elsewhere.
-      AWS.config.region = '<region>';
-
-      AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-          IdentityPoolId : '...', // your identity pool id here
-          Logins : {
-              // Change the key below according to the specific region your user pool is in.
-              'cognito-idp.<region>.amazonaws.com/<YOUR_USER_POOL_ID>' : result.getIdToken().getJwtToken()
-          }
-      });
-
-      //refreshes credentials using AWS.CognitoIdentity.getCredentialsForIdentity()
-      AWS.config.credentials.refresh((error) => {
-          if (error) {
-               console.error(error);
-          } else {
-               // Instantiate aws sdk service objects now that the credentials have been updated.
-               // example: var s3 = new AWS.S3();
-               console.log('Successfully logged!');
-          }
-      });
-  },
-
-  onFailure: function(err) {
-      alert(err.message || JSON.stringify(err));
-  },
-
-});
