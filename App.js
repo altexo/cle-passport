a76b99af6b4541e5716343a9899200e5a278e280
Unknown <16171281@itculiacan.edu.mx>
2018-06-27 17:46:01 -0600

    ambiente para testear el api

--- App.js
+++ App.js
@@ -69,7 +69,7 @@ class ProfileScreen extends React.Component {
 }
 
 const App = createStackNavigator({
-  Authorize: {screen:HomeView},
+  Authorize: {screen:VerifyInfoScreen},
   Profile:{ screen:ProfileScreen},
   Selfie:{screen:showSelfieScreen},
   IdFront:{screen:verifyidFront},
